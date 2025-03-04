189c98fe3bcb8a333b2d5956493e58d6f3223f48
Rangga Wiraguna <ranggawiragunar@gmail.com>
2025-03-04 22:04:55 +0700

    Add Basic Material - Inline Function

--- Basic/27 - Inline Function/main.kt
+++ Basic/27 - Inline Function/main.kt
@@ -1,3 +1,16 @@
 fun main(){
-	// Inline Function (Reduce the overhead of function call)	
+	// Inline Function (Reduce the overhead of function call for Higher Order Function)
+	// Lambda will duplicate the code in the function
+	for(i in 1..10){
+		println(stringFormat{"Rangga Wiraguna($i)"})
+	}
+	println()
+	for(j in 1..10){
+		println(stringFormatTransform("Rangga Wiraguna($j)"){it.uppercase()})
+		println(stringFormatTransform("Rangga Wiraguna($j)"){it.lowercase()})
+	}
+	println()
 }
+
+inline fun stringFormat(value : () -> String) : String = "Value\t: ${value()}" 
+inline fun stringFormatTransform(value : String,  transform : (String) -> String) : String = "Value\t: ${transform(value)}" 
